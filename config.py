0ca93b315ff9310ac021e9ce7c32b63ff3be3076
Akshay Kshirsagar <kshirsagar1408@gmail.com>
2021-02-26 15:04:48 -0500

    Update config.py

--- config.py
+++ config.py
@@ -10,6 +10,6 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['kshirsagar1408@gmail.com']
+    ADMINS = ['YOUR_EMAIL_ID']
     POSTS_PER_PAGE = 3
     LANGUAGES= ['en', 'es']
